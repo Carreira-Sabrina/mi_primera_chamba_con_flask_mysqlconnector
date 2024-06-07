e90ef9aaee524d2cc131787669451bb3f2772d02
Carreira-Sabrina <sabrina-viviana-carreira@gmail.com>
2024-06-06 23:44:52 -0300

    Hago muchos commits porque cambio de idea muy rapido

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ from config_base_datos import cursor_base_de_datos
 aplicacion = Flask (__name__)
 
 
-#Rutas
+# __________________ FUNCIONES CRUD _____________________________ 
+#ESTA FUNCION PERMITE RECUPERAR LOS POSTS DE LA BASE DE DATOS
+def get_posts():
+    consulta = ("SELECT posts.id_post,usuarios.nombre,posts.titulo,posts.texto,posts.fecha FROM posts INNER JOIN usuarios ON posts.id_usuario = usuarios.id_usuario")
+    cursor_base_de_datos.execute(consulta)
+    resultado_consulta = cursor_base_de_datos.fetchall()
+    
+    #SOLO PARA DEBUG, NO ME QUEMÉS JAJAJAJA
+    for fila in resultado_consulta:
+        print(fila)
+        
+    return resultado_consulta
+
+
+# ________________ Rutas ___________________
 
 #index.html
 @aplicacion.route("/")
@@ -15,19 +29,6 @@ def index():
     posts_en_bd = get_posts()
     #EL SEGUNDO PARAMETRO SON LOS DATOS DE LA BD QUE PASO PARA CARGAR DINAMICAMENTE CON JINJA EN EL HTML
     return render_template("index.html",posts = posts_en_bd)
-
-
-#ESTA FUNCION PERMITE RECUPERAR LOS POSTS DE LA BASE DE DATOS
-def get_posts():
-    #consulta = ("SELECT * FROM posts")
-    consulta = ("SELECT posts.id_post,usuarios.nombre,posts.titulo,posts.texto,posts.fecha FROM posts INNER JOIN usuarios ON posts.id_usuario = usuarios.id_usuario")
-    cursor_base_de_datos.execute(consulta)
-    resultado_consulta = cursor_base_de_datos.fetchall()
-    
-    for fila in resultado_consulta:
-        print(fila)
-    #ME CONVENDRIA DEVOLVER EL RESULTADO DE LA CONSULTA???
-    return resultado_consulta
 
 
 
--- readme.md
+++ readme.md
@@ -3,14 +3,20 @@
 - clonar el repositorio de la forma preferida
 - Abrir una consola tipo cmd (no powershell por favor ! )
 - Crear un entorno virtual:
-        - python -m venv nombreentorno virtual
-- Entrar en el entorno virtual: 
-        - .\nombreentornovirtual\Scripts\activate
+          ```
+         python -m venv nombreentornovirtual
+          ```
+- Entrar en el entorno virtual:
+            ```
+                   .\nombreentornovirtual\Scripts\activate
+            ```
 - Instalar las dependencias:
-        - pip install -r requirements.txt
+           ``` 
+                   pip install -r requirements.txt
+            ```
 
 - También vas a necesitar una base de datos, está incluida en la carpeta dump_BD, se puede importar desde MySQL Workbench
 desde Administration/MANAGEMENT/Data Import/Restore
 
--Los parámetros de mi conexion están en el archivo config_base_datos, tus parámetros de conexión (especialmente el password)
+-Los parámetros de mi conexion están en el archivo *config_base_datos.py*, tus parámetros de conexión (especialmente el password)
 podrían cambiar
